a58ec2e1d35ed9e40eab0af0b0f58c6fa1564d4d
BasarEkinci <basarekinci02@outlook.com>
2023-08-29 17:39:24 +0300

    Stick Stab added

--- 3DAA/Assets/GameFolders/_Scripts/Stick.cs
+++ 3DAA/Assets/GameFolders/_Scripts/Stick.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
+    [SerializeField] Transform stickSpawnPoint;
+    [SerializeField] GameObject stick;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(Vector3.down * (1000 * Time.deltaTime),ForceMode.Impulse);
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
             transform.SetParent(other.collider.transform);
+            rb.velocity = Vector3.zero;
+            
+            
         }
     }
 }
